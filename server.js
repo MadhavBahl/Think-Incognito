a2e58d50d68d6b0d7514506b4e05af532c979054
MADHAV BAHL <madhavbahl10@gmail.com>
2017-10-16 09:46:17 +0530

    Frontend Complete

--- server.js
+++ server.js
@@ -5,17 +5,32 @@ var path = require('path');
 var socket = require('socket.io');
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/thinkincognito');
 var port = process.env.PORT || 8080;
 var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'views')));
 
-app.get('/',(req,res) => {
-    res.render('index.hbs');
-});
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    fingerprint: {
+        type: String,
+        required: true
+    }
+})
+
+var User = mongoose.model('User',userSchema);
 
 var server = app.listen(port);
 console.log(`Server is up on port ${port}`);
+
+app.get('/',(req,res) => {
+    res.render('index.hbs');
+});
 
 var io = socket(server);
 
